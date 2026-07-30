9d87f1a2339e4482ead1c89a5d011e70b977b09d
hackingyseguridad <antonio_taboada@hotmail.es>
2026-07-30 13:44:41 +0200

    Modify deactivaX.sh to comment out systemctl command
    Commented out the command to set the default target to multi-user mode and added instructions for reverting the graphical mode.

--- deactivaX.sh
+++ deactivaX.sh
@@ -2,5 +2,15 @@
 # Simple Script en Bash Shell 1.0.x., desactiva modo x, modo grafico en Kali Linux,  en Linux Debian
 
 echo "Desactiva modo x, modo grafico en Kali Linux"
+sleep 3
+echo
+echo "Ctrol+C,  si no quieres deactivar el modo grafico X"
+sleep 9
 
-systemctl set-default multi-user.target
+# systemctl set-default multi-user.target
+
+echo
+echo "desactivado modo grafico!!"
+echo "si quieres revertir el modo X"
+echo "sudo systemctl start graphical.target"
+echo
