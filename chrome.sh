d9a73fc6c87100145c261d9b8dbf00b4f9e29f7e
hackingyseguridad <antonio_taboada@hotmail.es>
2026-09-03 11:20:14 +0200

    Update chrome.sh

--- chrome.sh
+++ chrome.sh
@@ -1,3 +1,10 @@
+#!/bin/sh
+# (r) hacking y seguridad .com 2026 Version Beta - Antonio Taboada 
+echo "###############################################################"
+echo "Ejecutando el navegador google Chrome ... google-chrome-stable --no-sandbox --disable-dev-shm-usage "
+echo "###############################################################"
+echo
+google-chrome-stable --no-sandbox --user-data-dir --disable-dev-shm-usage 
 
 
-google-chrome-stable --no-sandbox --disable-dev-shm-usage
+
